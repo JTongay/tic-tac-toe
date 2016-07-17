855ca91a61df241fcf84adae59b4af517f4ea270
Joseph Tongay <dirk.tongay@gmail.com>
2016-07-17 16:04:56 -0500

    fully functional

--- js/app.js
+++ js/app.js
@@ -4,31 +4,43 @@
 
     var player = 1;
 
-    $('.square').on('click', function (event) {
+    //Click events
+    $('.square').on('click', playGame);
+    $(document).on('click', '#newbutton', newGame);
+
+
+    // Game playing functionality
+    function playGame() {
       var squareSelected = $(this);
       if (squareSelected.hasClass('fa fa-times') || squareSelected.hasClass('fa fa-circle-o')) {
         alert("This square has already been selected! Please select another!");
       } else {
         if (player === 1) {
           squareSelected.addClass('fa fa-times');
+          $('#p1').show();
+          $('#p2').hide();
           if (checkIfPlayerWon('fa fa-times')) {
             alert("Congrats! Player " + player + " has won!")
           } else {
             player = 2;
           }
 
-        } else {
+        } else if (player === 2) {
           squareSelected.addClass('fa fa-circle-o');
+          $('#p1').hide();
+          $('#p2').show();
           if (checkIfPlayerWon('fa fa-circle-o')) {
             alert("Congrats! Player " + player + " has won!")
           } else {
             player = 1;
           }
-
         }
-      }
-    })
 
+      }
+    };
+
+
+    //Checks to see if there's a winner
     function checkIfPlayerWon(symbol) {
       if ($('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)) {
         return true;
@@ -49,8 +61,14 @@
       } else {
         return false;
       }
+    };
+
+    //Resets the gameboard
+    function newGame() {
+      $('.square').removeClass('fa fa-circle-o');
+      $('.square').removeClass('fa-times');
     }
 
-  })
+  });
 
 })();
